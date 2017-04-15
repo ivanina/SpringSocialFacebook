9be986253e0db6f924ffd45810987d8f87fe9171
Illya Ivanina <Illya_Ivanina@epam.com>
2017-04-15 14:35:51 +0300

    finished. not working on view, but on backend is well

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,9 +1,14 @@
 package hello;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.social.connect.Connection;
+import org.springframework.social.connect.ConnectionData;
 import org.springframework.social.connect.ConnectionRepository;
 import org.springframework.social.facebook.api.Facebook;
 import org.springframework.social.facebook.api.PagedList;
 import org.springframework.social.facebook.api.Post;
+import org.springframework.social.facebook.api.User;
+import org.springframework.social.facebook.connect.FacebookConnectionFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +18,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class HelloController {
     private Facebook facebook;
-    ConnectionRepository connection;
+    private ConnectionRepository connection;
+    private FacebookConnectionFactory facebookConnectionFactory;
+
+    @Value("${spring.social.facebook.appId}")
+    String fbAppId;
+    @Value("${spring.social.facebook.appSecret}")
+    String fbAppSecret;
 
     public HelloController(Facebook facebook, ConnectionRepository connection) {
         this.facebook = facebook;
@@ -25,9 +36,23 @@ public class HelloController {
         if(connection.findConnections(Facebook.class) == null){
             return "redirect:/connect/facebook";
         }
-        model.addAttribute("facebookProfile", facebook.userOperations().getUserProfile());
-        PagedList<Post> feed = facebook.feedOperations().getFeed();
-        model.addAttribute("feed", feed);
+
+        /*Connection<Facebook> connection = facebookConnectionFactory.createConnection(
+                new ConnectionData("facebook","738140579","","","","","","",null));
+        Facebook facebook = connection.getApi();
+        String [] fields = { "id", "email",  "first_name", "last_name" };
+        User userProfile = facebook.fetchObject("me", User.class, fields);*/
+
+
+
+        try {
+            model.addAttribute("facebookProfile", facebook.userOperations().getUserProfile());
+            PagedList<Post> feed = facebook.feedOperations().getFeed();
+            model.addAttribute("feed", feed);
+        }catch (Exception e){
+
+        }
+
         return "hello";
     }
 }
